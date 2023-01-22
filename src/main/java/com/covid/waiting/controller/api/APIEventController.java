dac6d49c483bc2dde90f83b722fb519233dbcd52
MoonStar <moonsung0331@gmail.com>
2023-01-22 21:13:14 +0900

    #2 - API Controllers
    View Controller 의 골격을 작성

---
+++ src/main/java/com/covid/waiting/controller/api/APIEventController.java
@@ -0,0 +1,30 @@
+package com.covid.waiting.controller.api;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping("/api")
+@RestController
+public class APIEventController {
+
+    @GetMapping("/events")
+    public List<String> getEvents() {
+        return List.of("event1", "event2");
+    }
+
+    @PostMapping("/events")
+    public Boolean createEvent() {
+        return true;
+    }
+
+    @GetMapping("/events/{eventId}")
+    public Boolean modifyEvent(@PathVariable Integer eventId) {
+        return true;
+    }
+
+    @DeleteMapping("/events/{eventId}")
+    public Boolean removeEvent(@PathVariable Integer eventId) {
+        return true;
+    }
+}
