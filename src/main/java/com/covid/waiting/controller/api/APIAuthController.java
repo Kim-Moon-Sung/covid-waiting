7384a2ea89ad0456a484fc15aa7e431bb3409a30
MoonStar <moonsung0331@gmail.com>
2023-01-23 18:33:36 +0900

    #2 - APIAuthController, DTO and its Test
    'APIAuthController' 테스트 작성 및 기본 입출력 설계
    어드민 회원가입, 로그인에 사용할 데이터 클래스 작성

--- src/main/java/com/covid/waiting/controller/api/APIAuthController.java
+++ src/main/java/com/covid/waiting/controller/api/APIAuthController.java
@@ -1,20 +1,21 @@
 package com.covid.waiting.controller.api;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.covid.waiting.dto.APIDataResponse;
+import com.covid.waiting.dto.AdminRequest;
+import com.covid.waiting.dto.LoginRequest;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/api")
 @RestController
 public class APIAuthController {
 
-    @GetMapping("/sign-up")
-    public String signUp() {
-        return "done.";
+    @PostMapping("/sign-up")
+    public APIDataResponse<String> signUp(@RequestBody AdminRequest adminRequest) {
+        return APIDataResponse.empty();
     }
 
-    @GetMapping("/login")
-    public String login() {
-        return "done.";
+    @PostMapping("/login")
+    public APIDataResponse<String> login(@RequestBody LoginRequest loginRequest) {
+        return APIDataResponse.empty();
     }
 }
