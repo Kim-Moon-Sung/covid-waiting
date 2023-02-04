c274ea4ab5f9567ed293f814f885efa2e30f15c9
MoonStar <moonsung0331@gmail.com>
2023-02-04 14:05:56 +0900

    #5 - EventDTO 가 이벤트 엔티티를 다루도록 업데이트
    'EventDTO' 가 'Event' 엔티티와 몇 가지 상황에서 상호작용할 수 있도록 기능을 추가

--- src/main/java/com/covid/waiting/dto/EventDTO.java
+++ src/main/java/com/covid/waiting/dto/EventDTO.java
@@ -1,6 +1,7 @@
 package com.covid.waiting.dto;
 
 import com.covid.waiting.constant.EventStatus;
+import com.covid.waiting.domain.Event;
 
 import java.time.LocalDateTime;
 
@@ -44,5 +45,47 @@ public record EventDTO(
                 createdAt,
                 modifiedAt
         );
+    }
+
+    public static EventDTO of(Event event) {
+        return new EventDTO(
+                event.getId(),
+                event.getPlaceId(),
+                event.getEventName(),
+                event.getEventStatus(),
+                event.getEventStartDatetime(),
+                event.getEventEndDatetime(),
+                event.getCurrentNumberOfPeople(),
+                event.getCapacity(),
+                event.getMemo(),
+                event.getCreatedAt(),
+                event.getModifiedAt()
+        );
+    }
+
+    public Event toEntity() {
+        return Event.of(
+                placeId,
+                eventName,
+                eventStatus,
+                eventStartDatetime,
+                eventEndDatetime,
+                currentNumberOfPeople,
+                capacity,
+                memo
+        );
+    }
+
+    public Event updateEntity(Event event) {
+        if (placeId != null) { event.setPlaceId(placeId); }
+        if (eventName != null) { event.setEventName(eventName); }
+        if (eventStatus != null) { event.setEventStatus(eventStatus); }
+        if (eventStartDatetime != null) { event.setEventStartDatetime(eventStartDatetime); }
+        if (eventEndDatetime != null) { event.setEventEndDatetime(eventEndDatetime); }
+        if (currentNumberOfPeople != null) { event.setCurrentNumberOfPeople(currentNumberOfPeople); }
+        if (capacity != null) { event.setCapacity(capacity); }
+        if (memo != null) { event.setMemo(memo); }
+
+        return event;
     }
 }
