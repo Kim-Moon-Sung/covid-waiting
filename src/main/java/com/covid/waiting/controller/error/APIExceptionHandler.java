79b3fa7ee4e253ae74be97deb41fe099e02799e3
MoonStar <moonsung0331@gmail.com>
2023-02-04 14:04:57 +0900

    #5 - APIExceptionHandler 에 spring data rest controllers 추가
    줄서기 서비스 API 에러 처리 핸들러가 spring data rest 에서 만든 컨트롤러도 인식할 수 있도록 설정을 추가하였음.

--- src/main/java/com/covid/waiting/controller/error/APIExceptionHandler.java
+++ src/main/java/com/covid/waiting/controller/error/APIExceptionHandler.java
@@ -3,6 +3,7 @@ package com.covid.waiting.controller.error;
 import com.covid.waiting.constant.ErrorCode;
 import com.covid.waiting.dto.APIErrorResponse;
 import com.covid.waiting.exception.GeneralException;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,7 +15,7 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 import javax.validation.ConstraintViolationException;
 
-@RestControllerAdvice(annotations = RestController.class)
+@RestControllerAdvice(annotations = {RestController.class, RepositoryRestController.class})
 public class APIExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler
