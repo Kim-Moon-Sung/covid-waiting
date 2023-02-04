3192921ade50e5de060498afc3d9baab215308f4
MoonStar <moonsung0331@gmail.com>
2023-02-04 13:57:23 +0900

    #5 - EventRepository 리팩토링
    제대로 추가되지 않은 import 문을 추가

--- src/main/java/com/covid/waiting/repository/EventRepository.java
+++ src/main/java/com/covid/waiting/repository/EventRepository.java
@@ -1,8 +1,12 @@
 package com.covid.waiting.repository;
 
 import com.covid.waiting.domain.Event;
+import com.covid.waiting.domain.QEvent;
+import com.querydsl.core.types.dsl.ComparableExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
 import org.springframework.data.querydsl.binding.QuerydslBindings;
 
 public interface EventRepository extends
@@ -14,7 +18,7 @@ public interface EventRepository extends
     default void customize(QuerydslBindings bindings, QEvent root) {
         bindings.excludeUnlistedProperties(true);
         bindings.including(root.placeId, root.eventName, root.eventStatus, root.eventStartDatetime, root.eventEndDatetime);
-        bindings.bind(root.eventName).first(StringExpression::likeIgnoreCase);
+        bindings.bind(root.eventName).first((StringExpression::likeIgnoreCase));
         bindings.bind(root.eventStartDatetime).first(ComparableExpression::goe);
         bindings.bind(root.eventEndDatetime).first(ComparableExpression::loe);
     }
