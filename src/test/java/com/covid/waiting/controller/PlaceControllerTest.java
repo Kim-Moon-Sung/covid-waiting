72fdf58e12a8bbfb29261cdf651f4de7ed04f88a
MoonStar <moonsung0331@gmail.com>
2023-02-07 01:26:13 +0900

    #6 - PlaceService & PlaceController 구현
    이벤트 컨트롤러와 서비스 만든 것과 동일한 구조로 장소 컨트롤러, 서비스 로직을 구현

--- src/test/java/com/covid/waiting/controller/PlaceControllerTest.java
+++ src/test/java/com/covid/waiting/controller/PlaceControllerTest.java
@@ -1,12 +1,21 @@
 package com.covid.waiting.controller;
 
+import com.covid.waiting.dto.PlaceDTO;
+import com.covid.waiting.service.PlaceService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -15,6 +24,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class PlaceControllerTest {
 
     private final MockMvc mvc;
+    @MockBean private PlaceService placeService;
 
     public PlaceControllerTest(@Autowired MockMvc mvc) {
         this.mvc = mvc;
@@ -24,24 +34,49 @@ public class PlaceControllerTest {
     @Test
     void givenNothing_whenRequestingPlacesPage_thenReturnsPlacesPage() throws Exception {
         //given
+        given(placeService.getPlaces(any())).willReturn(List.of());
 
         //when & then
         mvc.perform(get("/places"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
-                .andExpect(view().name("place/index"));
+                .andExpect(view().name("place/index"))
+                .andExpect(model().hasNoErrors())
+                .andExpect(model().attributeExists("places"));
+        then(placeService).should().getPlaces(any());
     }
 
     @DisplayName("[view][GET] 장소 상세 페이지")
     @Test
-    void givenNothing_whenRequestingPlaceDetailPage_thenReturnsPlaceDetailPage() throws Exception {
+    void givenPlaceId_whenRequestingPlaceDetailPage_thenReturnsPlaceDetailPage() throws Exception {
         //given
         long placeId = 1L;
+        given(placeService.getPlace(placeId)).willReturn(Optional.of(
+                PlaceDTO.of(null, null, null, null, null, null, null, null, null)
+        ));
 
         //when & then
         mvc.perform(get("/places/" + placeId))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
-                .andExpect(view().name("place/detail"));
+                .andExpect(view().name("place/detail"))
+                .andExpect(model().hasNoErrors())
+                .andExpect(model().attributeExists("place"));
+        then(placeService).should().getPlace(placeId);
+    }
+
+    @DisplayName("[view][GET] 장소 세부 정보 페이지 - 데이터 없음")
+    @Test
+    void givenNonexistentPlaceId_whenRequestingPlaceDetailPage_thenReturnsErrorPage() throws Exception {
+        // given
+        long placeId = 0L;
+        given(placeService.getPlace(placeId)).willReturn(Optional.empty());
+
+        // then & then
+        mvc.perform(get("/places/" + placeId))
+                .andExpect(status().isNotFound())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("error"));
+        then(placeService).should().getPlace(placeId);
     }
 }
